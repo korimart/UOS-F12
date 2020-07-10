e1334de5e330c423bc48064d8fbcbd9ba0c5a71e
korimart <korimart2019@gmail.com>
2020-07-11 07:27:46 +0900

    외국어강의 추가

---
+++ app/src/main/java/com/korimart/f12/CourseListFetcher.java
@@ -0,0 +1,61 @@
+package com.korimart.f12;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum CourseListFetcher {
+    INSTANCE;
+
+    private XMLHelper xmlHelper = XMLHelper.INSTANCE;
+
+    public static class CourseInfo {
+        String name;
+        String classification;
+        String classNumber;
+        int schoolYear;
+        int points;
+        String timePlace;
+        String professor;
+        boolean outsider;
+        boolean doubleMajor;
+        boolean minor;
+        boolean nonKorean;
+    }
+
+    public static class Result {
+        List<CourseInfo> courseInfos;
+        ErrorInfo errorInfo;
+    }
+
+    public void parse(Document doc, Result result){
+        result.courseInfos = new ArrayList<>();
+
+        NodeList lists = doc.getElementsByTagName("list");
+        for (int i = 0; i < lists.getLength(); i++) {
+            Node list = lists.item(i);
+            if (list.getNodeType() != Node.ELEMENT_NODE)
+                continue;
+
+            Element listEl = (Element) list;
+            CourseInfo courseInfo = new CourseInfo();
+            courseInfo.name = xmlHelper.getContentByName(listEl, "curi_nm");
+            courseInfo.classification = xmlHelper.getContentByName(listEl, "cmp_div_nm");
+            courseInfo.classNumber = xmlHelper.getContentByName(listEl, "class_no");
+            courseInfo.schoolYear = Integer.parseInt(xmlHelper.getContentByName(listEl, "cmp_shyr"));
+            courseInfo.points = Integer.parseInt(xmlHelper.getContentByName(listEl, "pnt"));
+            courseInfo.timePlace = xmlHelper.getContentByName(listEl, "lec_eng_nm");
+            courseInfo.professor = xmlHelper.getContentByName(listEl, "prof_nm");
+            courseInfo.outsider = xmlHelper.getContentByName(listEl, "etc_sect_permit_yn").equals("Y");
+            courseInfo.doubleMajor = xmlHelper.getContentByName(listEl, "sec_sect_permit_yn").equals("Y");
+            courseInfo.minor = xmlHelper.getContentByName(listEl, "minor_sect_permit_yn").equals("Y");
+            courseInfo.nonKorean = xmlHelper.getContentByName(listEl, "lsn_type_cd2").equals("외국어강의");
+
+            result.courseInfos.add(courseInfo);
+        }
+    }
+}
---
+++ app/src/test/java/com/korimart/f12/CourseListUnitTest.java
@@ -0,0 +1,136 @@
+package com.korimart.f12;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static com.korimart.f12.CourseListFetcher.*;
+
+public class CourseListUnitTest {
+    TestHelper testHelper = TestHelper.INSTANCE;
+
+    @Test
+    public void parserTest(){
+        String docString = testHelper.loadDocument("courseList2.xml", "euc-kr");
+        Document doc = XMLHelper.INSTANCE.getDocument(docString, "euc-kr");
+
+        Result result = new Result();
+        CourseListFetcher.INSTANCE.parse(doc, result);
+
+        CourseInfo info1 = new CourseInfo(){{
+            name = "창의공학기초설계";
+            classification = "전공필수";
+            classNumber = "02";
+            schoolYear = 1;
+            points = 3;
+            timePlace = "Fri 10,11,12,13/2-114";
+            professor = "이동희";
+            outsider = true;
+            doubleMajor = true;
+            minor = false;
+            nonKorean = false;
+        }};
+
+        CourseInfo info2 = new CourseInfo(){{
+            name = "창의공학기초설계";
+            classification = "전공필수";
+            classNumber = "03";
+            schoolYear = 1;
+            points = 3;
+            timePlace = "Thu 10,11,12,13/2-114";
+            professor = "현철승";
+            outsider = false;
+            doubleMajor = true;
+            minor = false;
+            nonKorean = false;
+        }};
+
+        CourseInfo info3 = new CourseInfo(){{
+            name = "학업설계상담 Ⅱ";
+            classification = "전공필수";
+            classNumber = "01";
+            schoolYear = 1;
+            points = 0;
+            timePlace = "";
+            professor = "홍의경";
+            outsider = false;
+            doubleMajor = false;
+            minor = false;
+            nonKorean = false;
+        }};
+
+        assertHasEntry(result.courseInfos, info1);
+        assertHasEntry(result.courseInfos, info2);
+        assertHasEntry(result.courseInfos, info3);
+    }
+
+    @Test
+    public void parserTest2(){
+        String docString = testHelper.loadDocument("courseList3.xml", "euc-kr");
+        Document doc = XMLHelper.INSTANCE.getDocument(docString, "euc-kr");
+
+        Result result = new Result();
+        CourseListFetcher.INSTANCE.parse(doc, result);
+
+        CourseInfo info1 = new CourseInfo(){{
+            name = "공학수학Ⅱ";
+            classification = "전공필수";
+            classNumber = "02";
+            schoolYear = 2;
+            points = 3;
+            timePlace = "Wed 06,07,08/19-B114/115";
+            professor = "안도열";
+            outsider = false;
+            doubleMajor = true;
+            minor = false;
+            nonKorean = true;
+        }};
+
+        CourseInfo info2 = new CourseInfo(){{
+            name = "알고리듬";
+            classification = "전공선택";
+            classNumber = "01";
+            schoolYear = 3;
+            points = 3;
+            timePlace = "Mon 07/19-216/217, Mon 08,09/19-104/105";
+            professor = "안영아";
+            outsider = false;
+            doubleMajor = true;
+            minor = false;
+            nonKorean = false;
+        }};
+
+        assertHasEntry(result.courseInfos, info1);
+        assertHasEntry(result.courseInfos, info2);
+    }
+
+    private void assertHasEntry(List<CourseInfo> infos, CourseInfo info){
+        CourseInfo infoReal1 = findEntryByNameNumber(infos, info.name, info.classNumber);
+        assertNotNull(infoReal1);
+        assertCourseInfoSame(infoReal1, info);
+    }
+
+    private CourseInfo findEntryByNameNumber(List<CourseInfo> infos, String name, String classNo){
+        for (CourseInfo currInfo : infos){
+            if (currInfo.name.equals(name) && currInfo.classNumber.equals(classNo))
+                return currInfo;
+        }
+        return null;
+    }
+
+    private void assertCourseInfoSame(CourseInfo info1, CourseInfo info2){
+       assertEquals(info1.name, info2.name);
+       assertEquals(info1.classification, info2.classification);
+       assertEquals(info1.classNumber, info2.classNumber);
+       assertEquals(info1.schoolYear, info2.schoolYear);
+       assertEquals(info1.points, info2.points);
+       assertEquals(info1.timePlace, info2.timePlace);
+       assertEquals(info1.professor, info2.professor);
+       assertEquals(info1.outsider, info2.outsider);
+       assertEquals(info1.doubleMajor, info2.doubleMajor);
+       assertEquals(info1.minor, info2.minor);
+       assertEquals(info1.nonKorean, info2.nonKorean);
+    }
+}
