2dc44f83a9fe636a400a5e34d1758ab88d8fd72e
korimart <korimart2019@gmail.com>
2020-07-16 09:23:21 +0900

    wrote and tested WParser

--- app/src/main/java/com/korimart/f12/SyllabusWParser.java
+++ app/src/main/java/com/korimart/f12/SyllabusWParser.java
@@ -1,10 +1,18 @@
 package com.korimart.f12;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SyllabusWParser implements WiseParser {
+public enum SyllabusWParser implements WiseParser {
+    INSTANCE;
+
+    private XMLHelper xmlHelper = XMLHelper.INSTANCE;
+
     public static class Result implements WiseParser.Result {
         public String summary;
         public String textbook;
@@ -17,8 +25,65 @@ public class SyllabusWParser implements WiseParser {
             return errorInfo;
         }
     }
+
     @Override
     public Result parse(Document doc) {
-        return null;
+        Result result = new Result();
+
+        result.summary = xmlHelper.getContentByName(doc, "lec_goal_descr");
+        result.textbook = xmlHelper.getContentByName(doc, "lec_shbk_descr");
+
+        if (result.summary == null || result.textbook == null){
+            result.errorInfo = new ErrorInfo(
+                    ErrorInfo.ErrorType.parseFailed,
+                    new Exception("summary or textbook was null")
+            );
+            return result;
+        }
+
+        result.summary = result.summary.trim();
+        result.textbook = result.textbook.trim();
+
+        result.weeklyPlans = new ArrayList<>();
+
+        NodeList lists = doc.getElementsByTagName("list");
+        if (lists.getLength() == 0){
+            result.errorInfo = new ErrorInfo(
+                    ErrorInfo.ErrorType.parseFailed,
+                    new Exception("no list"));
+            return result;
+        }
+
+        for (int i = 0; i < lists.getLength(); i++){
+            Node list = lists.item(i);
+            Element listElement = (Element) list;
+            String week = xmlHelper.getContentByName(listElement, "week_seq");
+            String plan = xmlHelper.getContentByName(listElement, "lec_descr");
+
+            if (week == null || plan == null){
+                result.errorInfo = new ErrorInfo(
+                        ErrorInfo.ErrorType.parseFailed,
+                        new Exception("week or plan was null")
+                );
+                return result;
+            }
+
+            try {
+                if (Integer.parseInt(week) != i + 1){
+                    result.errorInfo = new ErrorInfo(
+                            ErrorInfo.ErrorType.parseFailed,
+                            new Exception("weeks are not sequential")
+                    );
+                    return result;
+                }
+            } catch (NumberFormatException e){
+                result.errorInfo = new ErrorInfo(ErrorInfo.ErrorType.parseFailed, e);
+                return result;
+            }
+
+            result.weeklyPlans.add(plan.trim());
+        }
+
+        return result;
     }
 }
---
+++ app/src/test/java/com/korimart/f12/SyllabusParserTest.java
@@ -0,0 +1,71 @@
+package com.korimart.f12;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import static org.junit.Assert.*;
+
+public class SyllabusParserTest {
+    SyllabusOParser OParser = SyllabusOParser.INSTANCE;
+    SyllabusWParser WParser = SyllabusWParser.INSTANCE;
+    TestHelper testHelper = TestHelper.INSTANCE;
+    XMLHelper xmlHelper = XMLHelper.INSTANCE;
+
+    @Test
+    public void testOParser(){
+        byte[] rawDoc = testHelper.loadDocument("syllabusO.xml");
+        Document doc = xmlHelper.getDocument(rawDoc);
+
+        SyllabusOParser.Result result = OParser.parse(doc);
+
+        assertNull(result.errorInfo);
+        assertEquals("강의", result.lecPrac);
+        assertEquals("4학년", result.yearLevel);
+        assertEquals("전공선택(전공선택)", result.classification);
+        assertEquals("3학점(3시간)", result.pointsTime);
+        assertEquals("김민호", result.professor);
+        assertEquals("컴퓨터과학부", result.professorDept);
+        assertEquals("6490-2453", result.professorPhone);
+        assertEquals("minhokim@uos.ac.kr", result.professorEmail);
+        assertEquals("http://www.minho-kim.com", result.professorWeb);
+        assertEquals("", result.counseling);
+        assertEquals("상대평가", result.rubricsType);
+
+        assertEquals(4, result.rubrics.size());
+
+        assertEquals("출석", result.rubrics.get(0).first);
+        assertEquals("수시과제", result.rubrics.get(1).first);
+        assertEquals("중간과제", result.rubrics.get(2).first);
+        assertEquals("기말과제", result.rubrics.get(3).first);
+
+        assertEquals("10", result.rubrics.get(0).second);
+        assertEquals("10", result.rubrics.get(1).second);
+        assertEquals("40", result.rubrics.get(2).second);
+        assertEquals("40", result.rubrics.get(3).second);
+    }
+
+    @Test
+    public void testWParser(){
+        byte[] rawDoc = testHelper.loadDocument("syllabusW.xml");
+        Document doc = xmlHelper.getDocument(rawDoc);
+
+        SyllabusWParser.Result result = WParser.parse(doc);
+
+        assertNull(result.errorInfo);
+        assertEquals(
+                "-Shading language에 기초한 modern OpenGL을 이해하고 이를 사용하여 실시간 3차원 그래픽스 어플리케이션을 구현한다.\n" +
+                "-3차원 렌더링 알고리즘을 이해한다.\n" +
+                "-그래픽스 렌더링 파이프라인의 작동원리를 이해한다.\n" +
+                "-각종 3차원 그래픽스 관련 도구들의 사용법을 익힌다.", result.summary);
+        assertEquals(
+                "[optional] “WebGL Programming Guide” (by Kouichi Matsuda and Rodger Lea)",
+                result.textbook);
+        assertEquals(16, result.weeklyPlans.size());
+        assertEquals("introduction to JavaScript", result.weeklyPlans.get(0));
+        assertEquals("Ch03 Drawing w2ith OpenGL (1/2)", result.weeklyPlans.get(3));
+        assertEquals("Ch05 Viewing Transformations, Clipping, and Feedback", result.weeklyPlans.get(6));
+        assertEquals("보강주간", result.weeklyPlans.get(11));
+        assertEquals("OpenGL ES & WebGL", result.weeklyPlans.get(13));
+        assertEquals("Final presentations", result.weeklyPlans.get(15));
+    }
+}
